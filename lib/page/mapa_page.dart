5d1484814597c5cddf208869c5f6b9d23456492a
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2021-03-19 10:48:57 +0100

    [Dev] Añadido google maps
    Añadido google maps para obtener las geolocalizaciones escaneadas.

--- lib/page/mapa_page.dart
+++ lib/page/mapa_page.dart
@@ -1,16 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:qr_reader/providers/db_provider.dart';
 
-class MapaPage extends StatelessWidget {
-  const MapaPage({Key key}) : super(key: key);
-
+class MapaPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    final ScanModel scanModel = ModalRoute.of(context).settings.arguments;
+  _MapaPageState createState() => _MapaPageState();
+}
 
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  MapType mapType = MapType.normal;
+
+  Widget build(BuildContext context) {
+    final ScanModel scan = ModalRoute.of(context).settings.arguments;
+    final CameraPosition initialPoint =
+        CameraPosition(target: scan.getLatLng(), zoom: 17.4746, tilt: 50);
+
+    Set<Marker> markets = new Set<Marker>();
+    markets.add(new Marker(
+        markerId: MarkerId('geo-location'), position: scan.getLatLng()));
     return Scaffold(
-      body: Center(
-        child: Text(scanModel.value),
+      appBar: AppBar(
+        title: Text('Mapa'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () async {
+                final GoogleMapController controller = await _controller.future;
+                controller.animateCamera((CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                      target: scan.getLatLng(), zoom: 17.5, tilt: 50),
+                )));
+              })
+        ],
+      ),
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        mapType: mapType,
+        initialCameraPosition: initialPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: markets,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: () {
+          if (mapType == MapType.normal) {
+            mapType = MapType.satellite;
+          } else {
+            mapType = MapType.normal;
+          }
+          setState(() {});
+        },
       ),
     );
   }
--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -12,17 +12,17 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       child: Icon(Icons.filter_center_focus),
       onPressed: () async {
-        /*String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
             '#3D8BEF', 'Cancelar', false, ScanMode.QR);
-*/
-        String barcodeScanRes = 'http://www.google.es';
-        String barcodeScanGet = 'geo:12,34:12,125';
+
+        //String barcodeScanRes = 'http://www.google.es';
+        //String barcodeScanGet = 'geo:12,34:12,125';
         if (barcodeScanRes == '-1') {
           return;
         }
         final scanListProvider =
             Provider.of<ScanListProvider>(context, listen: false);
-        final newScan = await scanListProvider.newScan(barcodeScanGet);
+        final newScan = await scanListProvider.newScan(barcodeScanRes);
 
         launchURL(context, newScan);
       },
